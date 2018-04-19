43ac94573693bf9df2c5d879c9d9756c4bbe4ec4
Vazconcy <36010419+Vazconcy@users.noreply.github.com>
2018-04-19 18:59:18 -0300

    Add files via upload

---
+++ app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+
+
+import { AppComponent } from './app.component';
+import { FEMININOComponent } from './feminino/feminino.component';
+import { HeaderComponent } from './header/header.component';
+import { MasculinoComponent } from './masculino/masculino.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component'
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    RouterModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
